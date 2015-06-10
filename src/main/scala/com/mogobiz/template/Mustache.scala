a1c1eefc9b461a508c062d508b49996d48e7be60
Samy Dindane <samy@dindane.com>
2015-06-10 16:53:52 +0200

    Use Rhino for JDK < 8 and Nashorn for the rest

--- src/main/scala/com/mogobiz/template/Mustache.scala
+++ src/main/scala/com/mogobiz/template/Mustache.scala
@@ -3,14 +3,14 @@ package com.mogobiz.template
 import java.io.InputStreamReader
 import javax.script.{Invocable, ScriptEngine, ScriptEngineManager}
 
-/**
- * Created by hayssams on 27/11/14.
- */
 object Mustache {
   def apply(template: String, jsonString: String): String = {
+    val javaVersion = java.lang.Double.parseDouble(System.getProperty("java.specification.version"))
+    val engineName = if(javaVersion < 1.8) "rhino" else "nashorn"
+
     val manager: ScriptEngineManager = new ScriptEngineManager
     val engineManager: ScriptEngineManager = new ScriptEngineManager
-    val engine: ScriptEngine = engineManager.getEngineByName("rhino")
+    val engine: ScriptEngine = engineManager.getEngineByName(engineName)
     engine.eval(new InputStreamReader(this.getClass.getResourceAsStream("/template/mustache.js")))
     val invocable: Invocable = engine.asInstanceOf[Invocable]
     val json: AnyRef = engine.eval("JSON")
