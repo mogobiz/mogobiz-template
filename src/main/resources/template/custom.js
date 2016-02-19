3d631a547962bf9b60db35bd1bc8e2238843072c
Yoann Baudy <yoann.baudy@ebiznext.com>
2016-02-19 23:51:39 +0100

    JAHIAC-364 : Invoice template is not consistent with expectations

--- src/main/resources/template/custom.js
+++ src/main/resources/template/custom.js
@@ -11,7 +11,7 @@ function pay() {
 
 function formatDate() {
     return function (text, render) {
-        var patternList = ["yyyy", "yy", "MM", "dd", "HH", "mm", "ss"]
+        var patternList = ["yyyy", "yy", "MM", "dd", "HH", "hh", "mm", "ss", "a"]
         var s = render(text);
         var index = s.indexOf("|")
         if (index == -1) return s
@@ -31,16 +31,20 @@ function formatDate() {
                         var v = ""
                         if ("yyyy" == p) v = d.getFullYear()
                         else if ("yy" == p) v = d.getYear()
-                        else if ("MM" == p && d.getMonth() < 10) v = "0" + d.getMonth()
-                        else if ("MM" == p && d.getMonth() >= 10) v = d.getMonth()
+                        else if ("MM" == p && d.getMonth() < 9) v = "0" + (d.getMonth() + 1)
+                        else if ("MM" == p && d.getMonth() >= 9) v = (d.getMonth() + 1)
                         else if ("dd" == p && d.getDate() < 10) v = "0" + d.getDate()
                         else if ("dd" == p && d.getDate() >= 10) v = d.getDate()
                         else if ("HH" == p && d.getHours() < 10) v = "0" + d.getHours()
                         else if ("HH" == p && d.getHours() >= 10) v = d.getHours()
+                        else if ("hh" == p && d.getHours() < 10) v = "0" + (d.getHours() % 12)
+                        else if ("hh" == p && d.getHours() >= 10) v = (d.getHours() % 12)
                         else if ("mm" == p && d.getMinutes() < 10) v = "0" + d.getMinutes()
                         else if ("mm" == p && d.getMinutes() >= 10) v = d.getMinutes()
                         else if ("ss" == p && d.getSeconds() < 10) v = "0" + d.getSeconds()
                         else if ("ss" == p && d.getSeconds() >= 10) v = d.getSeconds()
+                        else if ("a" == p && d.getHours() < 12) v = "AM"
+                        else if ("a" == p && d.getHours() >= 12) v = "PM"
                         r = r.substring(0, index) + v + r.substring(index + p.length)
                     }
                 }
